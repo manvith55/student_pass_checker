6c501ff7937329c83f2f83e608ed2c397485cd7f
Manvith Kumar <manvithkumar55@gmail.com>
2025-03-05 15:28:54 +0530

    Update ticket.js

--- ticket.js
+++ ticket.js
@@ -9,33 +9,32 @@ checkButton.addEventListener("click", function () {
   let isAgeBelow20 = ageBelow20.checked;
   let hasStdId = stdId.checked;
 
-  let feedback;
-  if(isSeatAvailable){
+  let feedback = "";
+
+  if (isSeatAvailable) {
     console.log("You're Student");
-    feedback = "You're Student";
-    if(isAgeBelow20){
-      console.log(", Aged Under 20");
-      feedback = feedback + ", Aged Under 20";
-      if(hasStdId){
-        console.log("Has a Student IdCard");
-        feedback = feedback + ", Has a Student IdCard";
+    if (isAgeBelow20) {
+      console.log("Aged Under 20");
+      if (hasStdId) {
         if (confirm(`Are you sure? To take a ticket, click "OK"`)) {
-            alert("Congratulations 🥳🎉🎊 Your ticket is booked");
-        }else {
-            console.log(" you don't gey a ticket ");
-            feedback += ` "You don't get a ticket"`;
+          alert("Congratulations 🥳🎉🎊 Your ticket is booked");
+          console.log(`"Your ticket is booked"`);
+          feedback = "Your ticket is booked";
+        } else {
+          console.log(`"You don't get a ticket",If you want a ticket please Click "OK"`);
+          feedback = `"You don't get a ticket".<br>If you want a ticket Click "<b>OK<b>".`;
         }
-      }else{
-        console.log("but you don't have Student IdCard");
-        feedback = feedback + ", but you don't have Student IdCard";
+      } else {
+        console.log("But you don't have Student Identity Card");
+        feedback += "You don't have Student Identity Card";
       }
-    }else{
-      console.log("but your age isn't under 20 years");
-      feedback = feedback + ", but your age isn't under 20 years";
+    } else {
+      console.log("But your age isn't under 20 years");
+      feedback = feedback + "Your age isn't under 20 years";
     }
-  }else {
-      console.log ("You're not a student");
-      feedback = "You're not a student";
-    }
-    passengerStatus.textContent = feedback ;
+  } else {
+    console.log("You're not a Student");
+    feedback = "You're not a Student";
+  }
+  passengerStatus.innerHTML = feedback;
 });
